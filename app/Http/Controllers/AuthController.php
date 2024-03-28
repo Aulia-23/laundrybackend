59979994479870c94c281c1d581c1de77a30b3c1
Aulia-23 <aulianurhanaz@gmail.com>
2024-03-28 20:13:04 +0700

    tambah project backend

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Namshi\JOSE\JWT;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+		$credentials = $request->only('username', 'password');
+
+		try {
+			if(!$token = JWTAuth::attempt($credentials)){
+                return response()->json(['message' => 'Invalid username and password']);
+			}
+		} catch(JWTException $e){
+            return response()->json(['message' => 'Generate Token Failed']);
+		}
+
+		$user = JWTAuth::user();
+
+		return response()->json([
+			'success' => true,
+			'message' => 'Login berhasil',
+			'token'   => $token,
+			'user'    => $user
+		]);
+
+        // $data = [
+		// 	'token' => $token,
+		// 	'user'  => JWTAuth::user()
+		// ];
+        // return response()->json(['message' => 'Authentication success', 'data' => $data]);
+	}
+
+    public function loginCheck()
+    {
+        try {
+			if(!$user = JWTAuth::parseToken()->authenticate()){
+				return $this->response->errorResponse('Invalid token!');
+			}
+		} catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e){
+			return $this->response->errorResponse('Token expired!');
+		} catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e){
+			return $this->response->errorResponse('Invalid token!');
+		} catch (Tymon\JWTAuth\Exceptions\JWTException $e){
+			return $this->response->errorResponse('Token absent!');
+		}
+
+        return response()->json(['message' => 'Authentication success', 'user' => $user]);
+    }
+
+    public function logout(Request $request)
+    {
+        if(JWTAuth::invalidate(JWTAuth::getToken())) {
+            return response()->json(['message' => 'You are logged out']);
+        } else {
+            return response()->json(['message' => 'Failed to logout']);
+        }
+    }
+}
